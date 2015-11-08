34527ec94d469192af4dd598b10f66881fdd266b
LK <lenny.khazan@gmail.com>
2015-11-08 10:05:14 -0500

    Changed default screenshot location

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -39,3 +39,6 @@ defaults write com.apple.Safari IncludeDevelopMenu -bool true
 defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Change default screenshot location.
+defaults write com.apple.screencapture location ~/Desktop/Screenshots
